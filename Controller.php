a43003944d96c675a2e2c36aac5f8209ee502df7
EricJohn_Tanada <79491201+EricJohnTanada@users.noreply.github.com>
2021-04-30 08:36:14 -0700

    Update Controller.php

--- Controller.php
+++ Controller.php
@@ -7,10 +7,7 @@
       $functions = new Functions;
 
       echo "<br>".$functions -> findMaxRange($_GET['first'],$_GET['second']);
-      echo "<p>----------------------------------------------------------------------</p>";
       echo "<br>" . $functions -> countOnes($_GET['integer']);
-      echo "<p>----------------------------------------------------------------------</p>";
       echo "<br>" . $functions -> twoParts($_GET['path1'],$_GET['path2']);
-      echo "<p>----------------------------------------------------------------------</p>";
       echo "<br>" . $functions -> isSastryNumber($_GET['test']);
    ?>
